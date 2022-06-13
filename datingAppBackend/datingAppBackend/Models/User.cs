a894cc5c1ba67ad96f983799d52b8b52679c15a3
Ronel Cruz <ronel.cruz.a8@gmail.com>
2022-06-13 14:44:29 -0400

    adding nuggets packages

---
+++ datingAppBackend/datingAppBackend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace datingAppBackend.Models
+{
+	public class User
+	{
+		[Key]
+		public int id { get; set; }
+		public string name { get; set; }
+		public string lastName { get; set; }
+	}
+}
+
---
+++ datingAppBackend/datingAppBackend/Models/datingContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace datingAppBackend.Models
+{
+	public class datingContext:DbContext
+	{
+		public datingContext(DbContextOptions<datingContext>options):base(options)
+		{
+
+		}
+		public DbSet<User> Users { get; set; } = null;
+
+	}
+}
+
